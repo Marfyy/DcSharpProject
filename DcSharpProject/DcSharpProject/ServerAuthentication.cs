925c2d91ccbb95caf6ac2b1d1f301b85226d1a31
Martin Säfsten <martin@safsten.se>
2014-03-14 15:19:39 +0100

    Added classes for server connection
    Signed-off-by: Martin Säfsten <martin@safsten.se>

---
+++ DcSharpProject/DcSharpProject/ServerAuthentication.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DcSharpProject
+{
+    /// <summary>
+    /// Organizes the authentication to a dC# server
+    /// </summary>
+    class ServerAuthentication
+    {
+
+        public bool logonToServer(string serverIP, string port, string userName, string pw)
+        {
+            return false;
+        }
+        public bool logoutFromServer
+    }
+}
